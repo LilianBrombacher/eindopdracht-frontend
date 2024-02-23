0fa944ecc63dd49e5d8032422532f25bd13754d8
LilianBrombacher <lbrombacher91@gmail.com>
2024-02-23 15:15:50 +0100

    kleine aanpassing navbar en andere styling gedaan

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
     return (
     <main className= "page-container">
 
-        <header title= "WorldWide Office Hours"/>
+        <header className="title" title= "WorldWide Office Hours"/>
         <h1>WorldWide Office Hours</h1>
     </main>
 
