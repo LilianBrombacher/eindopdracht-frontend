32cf5d0b46ed1cd7d60da4f3b0964f0fab945edc
LilianBrombacher <lbrombacher91@gmail.com>
2024-02-25 15:24:23 +0100

    styling homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,27 @@
+import './Home.css'
+
+
 function Home() {
     return (
     <main className= "page-container">
+        {/*<span className="image-wrapper">*/}
+        {/*  /!*<header className="title" title= "WorldWide Office Hours"/>*!/*/}
+        {/*/!*<h1>WorldWide Office Hours</h1>*!/*/}
+        {/*/!*<img src={background} alt="worldmap"/>*!/*/}
+        {/*    </span>*/}
+        <header>
+        <div className="hero-content">
+            <h1 className="hero-title">Worldwide Office Hours</h1>
+            <p className="hero-subtitle">Is my colleague available right now?</p>
+        <form className="hero-form">
+            <label>
+                <input className="hero-input" type="text" placeholder="Type city"/>
+            </label>
+            <button type="button" className="hero-btn">Search</button>
+        </form>
+        </div>
+        </header>
 
-        <header className="title" title= "WorldWide Office Hours"/>
-        <h1>WorldWide Office Hours</h1>
     </main>
 
     )
